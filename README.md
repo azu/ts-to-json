22a867322a38081672873c966b339ebd00746c34
azu <azuciao@gmail.com>
2024-11-22 00:29:39 +0900

    init

---
+++ README.md
@@ -0,0 +1,58 @@
+# ts-to-json
+
+TypeScript `type` definition to JS
+
+## Install
+
+Install with [npm](https://www.npmjs.com/package/ts-to-json):
+
+    npm ci
+    npm test
+
+```js
+[
+  { name: 'ExamplePageA', path: '/example-page-a' },
+  {
+    name: 'ExamplePageB',
+    path: '/example-page-b',
+    params: { id: 'string' }
+  },
+  {
+    name: 'ExamplePageC',
+    path: '/example-page-c',
+    params: { id: 'string' }
+  },
+  { name: 'ExamplePageD', path: '/example-page-d', params: {} }
+]
+```
+
+
+## Changelog
+
+See [Releases page](https://github.com/azu/ts-to-json/releases).
+
+## Running tests
+
+Install devDependencies and Run `npm test`:
+
+    npm test
+
+## Contributing
+
+Pull requests and stars are always welcome.
+
+For bugs and feature requests, [please create an issue](https://github.com/azu/ts-to-json/issues).
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## Author
+
+- azu: [GitHub](https://github.com/azu), [Twitter](https://twitter.com/azu_re)
+
+## License
+
+MIT © azu
---
+++ src/parse.ts
@@ -0,0 +1,140 @@
+import * as ts from "typescript";
+import path from "node:path";
+
+/**
+ * typeの定義から、JSとして評価できる文字列を作成する
+ *
+ *
+ * 例: ; を , に変換する
+ *
+ * {
+ *     name: "ExamplePageA";
+ *     path: "/example-page-a";
+ * }
+ * ->
+ * {
+ *     name: "ExamplePageA",
+ *     path: "/example-page-a",
+ * }
+ * ----
+ *
+ * 例) stringを"string"に変換する
+ *
+ * {
+ *     name: "ExamplePageB";
+ *     path: "/example-page-b";
+ *     params: {
+ *         id: string;
+ *     };
+ * }
+ * ->
+ * {
+ *    name: "ExamplePageB",
+ *    path: "/example-page-b",
+ *    params: {
+ *      id: "string"
+ *    };
+ * }
+ * ----
+ *
+ * 例) union typeを最初の要素に変換する
+ *
+ * {
+ *     name: "ExamplePageC";
+ *     path: "/example-page-c";
+ *     params: {
+ *         id: "a" | "b" | "c";
+ *     };
+ * }
+ * ->
+ * {
+ *   name: "ExamplePageC",
+ *   path: "/example-page-c",
+ *   params: {
+ *        id: "a"
+ *   }
+ * }
+ * ----
+ *
+ * 例) optional typeを削除する
+ *
+ * {
+ *     name: "ExamplePageD";
+ *     path: "/example-page-d";
+ *     params: {
+ *       id?: string;
+ *     }
+ * }
+ * ->
+ * {
+ *    name: "ExamplePageD",
+ *    path: "/example-page-d",
+ *    params: {
+ *
+ *    }
+ *  }
+ * @param text
+ */
+function formatType(text: string) {
+    // 1. ; を , に変換する
+    text = text.replace(/;/g, ",");
+    // 2. stringを"string"に変換する
+    text = text.replace(/(\w+)\s{0,10}:\s{0,10}string/g, '"$1": "string"');
+    // 3. "a" | "b" | "c" ... を"a"に変換する
+    // | を見つけたら次の ; までを取得する、その中で最初の要素を取得する
+    text = text.replace(/(\w+):\s?".+?\|.+?"/g, '"$1": "$2"');
+    // 4. optional typeを削除する
+    text = text.replace(/(\w+)\?\s{0,10}:\s{0,10}(\w+),/g, "");
+    return eval(`(${text})`);
+}
+
+/** Generate documentation for all classes in a set of .ts files */
+function generateDocumentation(fileNames: string[], options: ts.CompilerOptions): void {
+    // Build a program using the set of root file names in fileNames
+    const program = ts.createProgram(fileNames, options);
+    // これがないとパーサーが起動できない
+    program.getTypeChecker();
+    // Get the checker, we will use it to find more about classes
+    const output: {
+        name: string;
+        path: string;
+        params: Record<string, string>;
+    }[] = [];
+
+    // Visit every sourceFile in the program
+    for (const sourceFile of program.getSourceFiles()) {
+        if (!sourceFile.isDeclarationFile) {
+            // Walk the tree to search for classes
+            ts.forEachChild(sourceFile, traverse);
+        }
+    }
+
+    // print out the doc
+    console.log(output);
+
+    return;
+
+    /** visit nodes finding exported classes */
+    function traverse(node: ts.Node) {
+        visit(node);
+
+        function visit(node: ts.Node) {
+            switch (node.kind) {
+                case ts.SyntaxKind.TypeAliasDeclaration: {
+                    const typeLiteral = (<ts.TypeAliasDeclaration>node).type;
+                    // unionは除外
+                    if (typeLiteral.kind === ts.SyntaxKind.TypeLiteral) {
+                        output.push(formatType(typeLiteral.getText()));
+                    }
+                }
+            }
+
+            ts.forEachChild(node, visit);
+        }
+    }
+}
+
+generateDocumentation([path.join(import.meta.dirname, "route.ts")], {
+    target: ts.ScriptTarget.ESNext,
+    module: ts.ModuleKind.NodeNext
+});
